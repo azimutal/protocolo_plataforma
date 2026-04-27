56f7163233e7e08add66a2073e3bcf596a4b365a
José Ángel <joseangelmt@outlook.com>
2026-04-27 14:27:26 +0200

    Eliminada refactorización que había hecho Visual Studio Code

--- Python/plataforma.py
+++ Python/plataforma.py
@@ -1,7 +1,7 @@
-from Python.constantes import *
-from Python.mensajes_control import * 
+from constantes import *
+from mensajes_control import * 
 from colorama import Fore
-from Python.conexion_platforma import ComunicacionControlPlataforma
+from conexion_platforma import ComunicacionControlPlataforma
 import struct
 
 COMANDO_ESTABLECER_VELOCIDAD = 1
