feeda29f2ae918446d2382fdab7937031b469334
José Ángel <joseangelmt@outlook.com>
2026-04-27 13:56:38 +0200

    Añadido un contador de hertzios

--- LeeBladesDetector/Program.cs
+++ LeeBladesDetector/Program.cs
@@ -10,14 +10,33 @@ if (args.Length < 1)
 var ip = args[0];
 using var cliente = new ClienteVisionArtificial(ip);
 
+
+var timestampUltimaImpresión = 0.0;
+var paquetesRecibidos = 0;
+object lockObj = new();
+var hz = 0.0;
+
 cliente.PaqueteRecibido += (_, d) =>
 {
-    Console.WriteLine(
-        $"[{d.Timestamp,12:F4}] " +
-        $"Yaw={d.Yaw,8:F4}  Pitch={d.Pitch,8:F4}  " +
-        $"Roll=({d.Roll0:F4}, {d.Roll1:F4}, {d.Roll2:F4})  " +
-        $"Eje4=({d.Cuarto0:F4}, {d.Cuarto1:F4}, {d.Cuarto2:F4})"
-    );
+    lock (lockObj)
+    {
+        paquetesRecibidos++;
+        var tiempoTranscurrido = d.Timestamp - timestampUltimaImpresión;
+        if (tiempoTranscurrido >= 1.0)
+        {
+            hz = paquetesRecibidos / tiempoTranscurrido;
+            timestampUltimaImpresión = d.Timestamp;
+            paquetesRecibidos = 0;
+        }
+
+        Console.WriteLine(
+            $"[{d.Timestamp,12:F4}] " +
+            $"Yaw={d.Yaw,8:F4}  Pitch={d.Pitch,8:F4}  " +
+            $"Roll=({d.Roll0:F4}, {d.Roll1:F4}, {d.Roll2:F4})  " +
+            $"Eje4=({d.Cuarto0:F4}, {d.Cuarto1:F4}, {d.Cuarto2:F4}) " +
+            $"Hz={hz,6:F2}"
+        );
+    }
 };
 
 cliente.Start();
